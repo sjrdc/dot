fee41ad6b08f85493225f5963d3e6a6f72b81ada
Sjoerd Crijns <s.crijns@umcutrecht.nl>
2016-04-04 15:03:54 +0200

    add git config

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 dotdir=$(dirname $(readlink -f $BASH_SOURCE))
+deploydir=~
 
 # deploy emacs stuff
-ln -s $dotdir/emacs/emacs.d ~
-ln -s $dotdir/emacs/.emacs ~
+ln -s $dotdir/emacs/emacs.d $deploydir
+ln -s $dotdir/emacs/.emacs $deploydir
 
 # deploy matlab startup script
-ln -s $dotdir/matlab/startup.m
+ln -s $dotdir/matlab/startup.m $deploydir
 
+# deploy git stuff
+ln -s $dotdir/git/.gitconfig $deploydir
