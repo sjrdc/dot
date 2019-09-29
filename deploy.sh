2a6f5bee664361f755cc88508b3229f169509150
sjrdc <scrijns@gmail.com>
2019-09-29 18:39:12 +0200

    Deploy recursively

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ type $REALPATH >/dev/null 2>&1 || \
 dotdir=$(dirname $(readlink -f $BASH_SOURCE))
 deploydir=$(realpath $HOME)
 
-deploycmd="$STOW --dir $dotdir --target $deploydir"
+deploycmd="$STOW -R --dir $dotdir --target $deploydir"
 
 type $STOW >/dev/null 2>&1 || \
     { echo >&2 "requires $STOW but it's not installed. Aborting."; \
@@ -23,6 +23,7 @@ type $STOW >/dev/null 2>&1 || \
 
 for d in $(ls $dotdir); do
     if [ -d $d ]; then
+	echo $deploycmd $d
 	$deploycmd $d 2>&1 
     fi
 done
