0b73cdaec8d26223007ee16d893cc785755342bd
sjrdc <scrijns@gmail.com>
2025-03-21 10:57:34 +0100

    Annotate script and split into sections

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
+# make sure we have the latest packages and index
 sudo apt update && sudo apt dist-upgrade -y
 
+# install terminal
 sudo apt install -y \
-     terminator \
+     terminator
+
+# install build tools
+sudo apt install -y \
      cmake \
      g++ \
      gdb \
      build-essential \
-     libboost-dev \
-     python3-pdm-pep517 \
-     python3-venv \
-     node-http-server \
-     emscripten \
-     xstow \
+     libboost-dev 
+
+# install IDE(s)
+
+sudo apt install -y \
      emacs 
 
+# install web dev items
+sudo apt install -y \
+     emscripten 
+
+# install python prerequisites
+sudo apt install -y \
+     python3-pdm-pep517 \
+     python3-venv 
+
+# configure python env 
 python3 -m venv ~/.local/env
 source ~/.local/env/activate
 pip install pdm 
